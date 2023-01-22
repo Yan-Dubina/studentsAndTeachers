3e6b8c501d59b431bc171574c92124e8b1c3ee76
babyan <babyan1997@gmail.com>
2023-01-22 20:56:33 +0100

    fix

--- src/main/java/com/example/shop/configurations/SecSecurityConfig.java
+++ src/main/java/com/example/shop/configurations/SecSecurityConfig.java
@@ -33,14 +33,6 @@ public class SecSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf()
                 .disable()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS, "/**")
-                .permitAll()
-                .antMatchers("/login")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
                 .httpBasic();;
         return http.build();
     }
